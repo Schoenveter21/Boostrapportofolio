bb63ebbb4c4bbeb6180f027410441dbdd85a8ff6
Schoenveter21 <156431267+Schoenveter21@users.noreply.github.com>
2024-04-25 09:29:04 +0200

    push

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mijn portofolio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-info ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Achraf's website</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Over mij</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Projecten
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Unity</a></li>
+            <li><a class="dropdown-item" href="#">Web</a></li>
+            <li><a class="dropdown-item" href="#">App</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Contact</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="Foto">
+  <img class="MijnFoto" src="foto's/MijnFoto.jpg" alt="Achraf's afbeelding" width="1863" height="400">
+  <h1 class="Welkom"> Welkom</h1>
+</div>
+
+<div class="container">
+  <div class="row justify-content-center mt-8">
+    <div class="col-md-8">
+      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec auctor mi, et ullamcorper est. Aliquam sodales est mi, eu fringilla ex cursus sit amet. Donec lacinia erat at turpis laoreet tincidunt. Sed vestibulum, nulla sed placerat dignissim, ipsum lorem lacinia erat, id posuere ante neque quis felis. Cras feugiat eros vel diam aliquam, a mattis magna hendrerit. Etiam consectetur purus sed metus dignissim, vel laoreet arcu sagittis. Vivamus sodales feugiat ex, nec sagittis ipsum facilisis non. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam ut felis mi. Aliquam quis tempor lacus, vitae eleifend velit. Etiam pulvinar orci viverra pharetra scelerisque.
+
+Sed ultricies enim quis tellus tempus condimentum. Donec lobortis pharetra urna. Etiam aliquam libero sit amet dolor consequat, nec ullamcorper quam bibendum. Pellentesque condimentum sed ipsum eget pulvinar. Mauris porta libero mattis, porttitor sem nec, efficitur massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu porttitor diam. Morbi quis sodales turpis, vel gravida massa. Proin at purus cursus, rutrum odio eu, pretium urna. In lacinia justo at magna condimentum, id euismod felis pulvinar. Vestibulum pellentesque posuere nisi. Nullam sollicitudin orci euismod, aliquet lorem nec, dignissim ipsum.
+Nulla urna ipsum, dictum id orci ac, lobortis luctus diam. Integer semper massa sed magna interdum rhoncus sit amet ac ante. Proin pellentesque porta dui, sit amet interdum enim convallis id. Nam vulputate est dapibus, elementum diam aliquam, porta risus. Integer imperdiet urna ut pharetra pretium. Nulla sit amet erat eget odio fringilla mollis. Praesent vitae tempus magna. Suspendisse ultricies odio turpis, eu egestas nunc gravida in. Etiam eu scelerisque justo. Sed iaculis justo vel mi commodo finibus. Sed facilisis, sem quis laoreet maximus, sapien arcu scelerisque turpis, quis blandit eros nunc sollicitudin dolor.</p>
+    </div>
+  </div>
+</div>
+<footer class="footer mt-auto py-3 bg-dark">
+  <div class="container2 text-center">
+    <span class="text-light">© 2024 Achraf's Website</span>
+  </div>
+</footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html> 
